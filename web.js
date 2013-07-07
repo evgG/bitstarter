93611dae28c89c31a3b9a8d09f4641fd898febad
Evguen Grabovetskyi <evgueniy.g@gmail.com>
2013-07-07 09:14:58 +0000

    Read index.html and print it out

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var readfile = fs.readFileSync('index.html', "ascii");
+var buf = new Buffer(readfile.length);
+buf.write(readfile, "ascii");
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(buf.toString("ascii"));
+
 });
 
 var port = process.env.PORT || 5000;
